93e7d8c6daf36f417e94d341ddcf63f4033858ed
Dominik Summa <dominik.summa@tgc-solutions.com>
2017-08-11 11:06:37 +0200

    Initial Maven project

---
+++ src/main/java/de/dosu411/DemoHib/App.java
@@ -0,0 +1,13 @@
+package de.dosu411.DemoHib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
