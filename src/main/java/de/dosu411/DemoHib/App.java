df3b89007b5e4ee3e5b2657107c8512cedae2c48
Dominik Summa <dominik.summa@gmx.de>
2017-08-13 21:07:50 +0200

    Finished Hibernate example 1.4 and 1.5

--- src/main/java/de/dosu411/DemoHib/App.java
+++ src/main/java/de/dosu411/DemoHib/App.java
@@ -1,13 +1,25 @@
 package de.dosu411.DemoHib;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+	Alien telusko = new Alien();
+	telusko.setAid(101);
+	telusko.setAname("Navin");
+	telusko.setColor("Green");
+
+	Configuration config = new Configuration();
+
+	SessionFactory sf = config.buildSessionFactory();
+	Session session = sf.openSession();
+
+	session.save(telusko);
     }
 }
